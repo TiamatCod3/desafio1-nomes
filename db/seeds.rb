83e8436f436fd88fb6ceb8edcb784c25d070f1a3
TiamatCod3 <ti_matos@id.uff.br>
2022-02-19 00:04:55 -0300

    Model creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# https://gist.github.com/arjunvenkat/1115bc41bf395a162084
+# https://gist.github.com/wrburgess/ea0869979accad8172d3
+require 'csv'
+
+# csv_text = File.read('alunos.csv')
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'alunos.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  student = Student.new
+  student.name = row['nome']
+  student.register = row['matricula']
+  student.phone = row['telefone']
+  student.mail = row['email']
+  student.uffmail = row['uffmail']
+  student.active = (row['status']=="Ativo")
+  student.save
+end
