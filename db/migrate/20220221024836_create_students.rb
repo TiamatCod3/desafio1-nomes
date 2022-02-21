daae759cd2159388e9777b219c7cd28a78fb8802
TiamatCod3 <ti_matos@id.uff.br>
2022-02-20 23:49:59 -0300

    model creation

---
+++ db/migrate/20220221024836_create_students.rb
@@ -0,0 +1,13 @@
+class CreateStudents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :register
+      t.string :phone
+      t.string :mail
+      t.string :uffmail
+
+      t.timestamps
+    end
+  end
+end
