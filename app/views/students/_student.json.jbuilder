e1c9205b71b5df04cc46a4dcb257ffe8e469b4d4
TiamatCod3 <ti_matos@id.uff.br>
2022-02-20 23:27:46 -0300

    Reset

--- app/views/students/_student.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! student, :id, :name, :register, :phone, :mail, :uffmail, :status, :created_at, :updated_at
-json.url student_url(student, format: :json)
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root to: "students#index"
-  resources :students
-  get 'search', to:"students#search"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
