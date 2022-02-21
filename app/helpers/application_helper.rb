38baa546e7438b24f7fd742ff852e101987cb7d6
TiamatCod3 <ti_matos@id.uff.br>
2022-02-21 00:36:59 -0300

    View fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def flash_class(level)
+    bootstrap_alert_class = {
+      "success" => "alert alert-success alert-dismissible",
+      "error" => "alert alert-warning alert-dismissible",
+      "notice" => "alert alert-warning alert-dismissible",
+      "alert" => "alert alert-warning alert-dismissible",
+      "warn" => "alert alert-warning alert-dismissible"
+    }
+    
+    bootstrap_alert_class[level]
+  end
 end
